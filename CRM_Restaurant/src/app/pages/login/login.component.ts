09316a1110b64b92988c62d2f8562463f18f1c81
AndryAlexis <andry.html@gmail.com>
2024-11-14 17:59:49 +0100

    Terminado

--- CRM_Restaurant/src/app/pages/login/login.component.ts
+++ CRM_Restaurant/src/app/pages/login/login.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  
+  constructor(private router: Router) {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)])
+    });
+  }
 
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form submitted', this.loginForm.value);
+    }
+  }
+
+  getPasswordErrorMessage(): string {
+    const passwordControl = this.loginForm.get('password');
+    if (passwordControl?.errors?.['required']) {
+      return 'Password is required';
+    }
+    return '';
+  }
+
+  checkValidation(field: string) {
+    return this.loginForm.get(field)?.invalid && this.loginForm.get(field)?.touched;
+  }
 }
