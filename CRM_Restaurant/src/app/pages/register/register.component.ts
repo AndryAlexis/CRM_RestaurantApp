58dc8f49744eab53c61851972a56480619e2dfb9
AndryAlexis <andry.html@gmail.com>
2024-11-14 18:06:32 +0100

    doing that

--- CRM_Restaurant/src/app/pages/register/register.component.ts
+++ CRM_Restaurant/src/app/pages/register/register.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
+  
+  constructor(private router: Router) {
+    this.registerForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)])
+    });
+  }
 
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Form submitted', this.registerForm.value);
+    }
+  }
+
+  getPasswordErrorMessage(): string {
+    const passwordControl = this.registerForm.get('password');
+    if (passwordControl?.errors?.['required']) {
+      return 'Password is required';
+    }
+    return '';
+  }
+
+  checkValidation(field: string) {
+    return this.registerForm.get(field)?.invalid && this.registerForm.get(field)?.touched;
+  }
 }
