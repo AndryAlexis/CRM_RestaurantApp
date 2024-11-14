9a3f6f53f3048fcf18a47f44e911d199d83d4624
AndryAlexis <andry.html@gmail.com>
2024-11-14 17:00:45 +0100

    Páginas públicas creadas

--- CRM_Restaurant/src/app/app.routes.ts
+++ CRM_Restaurant/src/app/app.routes.ts
@@ -1,3 +1,52 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { ReservationsComponent } from './pages/reservations/reservations.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: '', 
+    component: HomeComponent, 
+    title: 'Home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Register',
+    pathMatch: 'full'
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    title: 'Profile',
+    pathMatch: 'full'
+  },
+  {
+    path: 'menu',
+    component: MenuComponent,
+    title: 'Menu',
+    pathMatch: 'full'
+  },
+  {
+    path: 'reservations',
+    component: ReservationsComponent,
+    title: 'Reservations',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  
+];
