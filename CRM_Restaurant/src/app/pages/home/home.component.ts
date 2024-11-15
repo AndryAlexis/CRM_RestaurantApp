732f4eec38903fb5975eef6ef45bc439a4bd3eb8
FranFunesMolero <ffunesmolero@icloud.com>
2024-11-15 15:02:50 +0100

    Mejorado el header y home basico falta detalles

--- CRM_Restaurant/src/app/pages/home/home.component.ts
+++ CRM_Restaurant/src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { RouterLink } from '@angular/router';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,RouterLink],
+  imports: [HeaderComponent,RouterLink, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
