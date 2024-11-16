ad12435c96bf1278e763d6f2b909596052b9154f
noemesa93 <noemesa93@gmail.com>
2024-11-16 17:36:35 +0100

    menu dashboard

--- CRM_Restaurant/src/app/pages/menu-dashboard/menu-dashboard.component.ts
+++ CRM_Restaurant/src/app/pages/menu-dashboard/menu-dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderDashboardComponent } from "../../components/dashboard/header-dashboard/header-dashboard.component";
+import { MenuCardComponent } from "../../components/menu-card/menu-card.component";
 
 @Component({
   selector: 'app-menu-dashboard',
   standalone: true,
-  imports: [],
+  imports: [HeaderDashboardComponent, MenuCardComponent],
   templateUrl: './menu-dashboard.component.html',
   styleUrl: './menu-dashboard.component.css'
 })
