99a4c66e30254118e986c9f67b0e30b22d1b30ef
noemesa93 <noemesa93@gmail.com>
2024-11-18 11:40:18 +0100

    no message

--- CRM_Restaurant/src/app/pages/dashboard/menu-dashboard/view-menu/view-menu.component.ts
+++ CRM_Restaurant/src/app/pages/dashboard/menu-dashboard/view-menu/view-menu.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderDashboardComponent } from "../../../../components/dashboard/header-dashboard/header-dashboard.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-view-menu',
   standalone: true,
-  imports: [],
+  imports: [HeaderDashboardComponent, RouterLink],
   templateUrl: './view-menu.component.html',
   styleUrl: './view-menu.component.css'
 })
