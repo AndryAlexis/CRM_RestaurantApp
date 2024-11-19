004ca1cc557188403c8843fcca8fae15e4e03912
noemesa93 <noemesa93@gmail.com>
2024-11-19 12:25:37 +0100

    no message

--- src/app/pages/dashboard/reviews/reviews.component.ts
+++ src/app/pages/dashboard/reviews/reviews.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderDashboardComponent } from "../../../components/dashboard/header-dashboard/header-dashboard.component";
-
+import { ReviewCardComponent } from "../../../components/dashboard/review-card/review-card.component";
+import { RouterLink } from "@angular/router";
 @Component({
   selector: 'app-reviews',
   standalone: true,
-  imports: [HeaderDashboardComponent],
+  imports: [HeaderDashboardComponent, ReviewCardComponent, RouterLink],
   templateUrl: './reviews.component.html',
   styleUrl: './reviews.component.css'
 })
