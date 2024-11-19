3cfe3db7640690529f28f5f66f538df40b6d168c
FranFunesMolero <ffunesmolero@icloud.com>
2024-11-19 09:51:34 +0100

    craeada la pagina nueva en la rama develop hago commit y me cambio de rama, ha sido un despiste

---
+++ src/app/pages/dashboard/usuarios-registrados/usuarios-registrados.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usuarios-registrados',
+  standalone: true,
+  imports: [],
+  templateUrl: './usuarios-registrados.component.html',
+  styleUrl: './usuarios-registrados.component.css'
+})
+export class UsuariosRegistradosComponent {
+
+}
