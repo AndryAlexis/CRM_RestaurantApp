a662c919017539167ce104aac95548c60a639d60
noemesa93 <noemesa93@gmail.com>
2024-11-19 19:04:45 +0100

    cambio de colores botones y pagina de ver usuarios

--- src/app/pages/dashboard/menu/home/home.component.ts
+++ src/app/pages/dashboard/menu/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderDashboardComponent } from '../../../../components/dashboard/header-dashboard/header-dashboard.component';
 import { MenuCardComponent } from '../../../../components/dashboard/menu-card/menu-card.component';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderDashboardComponent, MenuCardComponent, RouterLink],
+  imports: [HeaderDashboardComponent, MenuCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/pages/dashboard/reviews/reviews.component.ts
+++ src/app/pages/dashboard/reviews/reviews.component.ts
@@ -13,18 +13,6 @@ import { ReviewCardComponent } from "../../../components/dashboard/review-card/r
 export class ReviewsComponent {
 
 
-  // Mostrar el botón que se muestra cuando se desplaza hacia abajo
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const button = document.getElementById('btn-top') as HTMLElement;
-    if (window.scrollY > 100) {
-      button.style.display = 'block';
-    } else {
-      button.style.display = 'none';
-    }
-  }
-
-  // Función para desplazarse hacia arriba el botón
   scrollToTop() {
     window.scrollTo({
       top: 0,
