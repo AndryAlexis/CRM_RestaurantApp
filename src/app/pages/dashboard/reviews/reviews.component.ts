7d9ecd57720ae943fc87eda67bcfa04d13d9c162
noemesa93 <noemesa93@gmail.com>
2024-11-19 17:51:01 +0100

    Cambiar botón volver de reviews a botón hacia top. Cambio prox. reservas. Boton crear menu cambiado.

--- src/app/pages/dashboard/reviews/reviews.component.ts
+++ src/app/pages/dashboard/reviews/reviews.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { HeaderDashboardComponent } from "../../../components/dashboard/header-dashboard/header-dashboard.component";
 import { ReviewCardComponent } from "../../../components/dashboard/review-card/review-card.component";
-import { RouterLink } from "@angular/router";
+
+
 @Component({
   selector: 'app-reviews',
   standalone: true,
-  imports: [HeaderDashboardComponent, ReviewCardComponent, RouterLink],
+  imports: [HeaderDashboardComponent, ReviewCardComponent],
   templateUrl: './reviews.component.html',
   styleUrl: './reviews.component.css'
 })
 export class ReviewsComponent {
 
+
+  // Mostrar el botón que se muestra cuando se desplaza hacia abajo
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const button = document.getElementById('btn-top') as HTMLElement;
+    if (window.scrollY > 100) {
+      button.style.display = 'block';
+    } else {
+      button.style.display = 'none';
+    }
+  }
+
+  // Función para desplazarse hacia arriba el botón
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
 }
