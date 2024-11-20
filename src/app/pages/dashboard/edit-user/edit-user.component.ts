6015c4d5ed0fc18aa16fcc89597ef022003719eb
FranFunesMolero <ffunesmolero@icloud.com>
2024-11-20 16:20:40 +0100

    añadida pagina editar usuario

--- src/app/pages/dashboard/edit-user/edit-user.component.ts
+++ src/app/pages/dashboard/edit-user/edit-user.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeaderDashboardComponent } from '../../../components/dashboard/header-dashboard/header-dashboard.component';
 
 @Component({
   selector: 'app-edit-user',
   standalone: true,
-  imports: [],
+  imports: [HeaderDashboardComponent],
   templateUrl: './edit-user.component.html',
   styleUrl: './edit-user.component.css'
 })
 export class EditUserComponent {
+  userId: string;
 
+  constructor(private route: ActivatedRoute) {
+    this.userId = this.route.snapshot.paramMap.get('id') || '';
+  }
 }
