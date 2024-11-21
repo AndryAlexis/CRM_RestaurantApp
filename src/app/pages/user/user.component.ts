e331330dc105a30aa4cc2ea1c30673f2225aae5e
FranFunesMolero <ffunesmolero@icloud.com>
2024-11-21 10:44:52 +0100

    pagina profile

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { FooterComponent } from '../../components/footer/footer.component';
 import { RouterLink } from '@angular/router';
+import { ReviewCardComponent } from "../../components/dashboard/review-card/review-card.component";
+import { ReservationsCardComponent } from "../../components/dashboard/reservations-card/reservations-card.component";
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterLink],
+  imports: [HeaderComponent, FooterComponent, RouterLink, ReviewCardComponent, ReservationsCardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
