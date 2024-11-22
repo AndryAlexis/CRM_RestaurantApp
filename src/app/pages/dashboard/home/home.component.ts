8b95191eaa8092e1cacc70f39848b50dcc85013e
gominada <gominada@proton.me>
2024-11-22 13:20:46 +0100

    Add Reservations functionality (fetch data)  & Refactor its design

--- src/app/pages/dashboard/home/home.component.ts
+++ src/app/pages/dashboard/home/home.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ReviewCardComponent } from "../../../components/dashboard/review-card/review-card.component";
 import { ReservationsCardComponent } from "../../../components/dashboard/reservations-card/reservations-card.component";
 import { HeaderDashboardComponent } from "../../../components/dashboard/header-dashboard/header-dashboard.component";
 import { RouterLink } from "@angular/router";
 import { NewUsersCardComponent } from "../../../components/dashboard/new-users-card/new-users-card.component";
+import { ReservationService } from '../../../services/reservation.service';
+import { ICustomerReservationResponse } from '../../../interfaces/icustomer-reservation-response.interface';
 
 @Component({
   selector: 'app-home',
@@ -14,4 +16,22 @@ import { NewUsersCardComponent } from "../../../components/dashboard/new-users-c
 })
 export class HomeComponent {
 
+  reservationService = inject(ReservationService)
+  reservationsToday: ICustomerReservationResponse[] = [];
+  reservations: ICustomerReservationResponse[] = [];
+  currentDate = new Date()
+  date: string = this.currentDate.toLocaleDateString('en-CA')
+
+  ngOnInit() {
+    this.loadReservations()
+  }
+
+  async loadReservations() {
+    this.date = '2024-11-25'
+    this.reservationsToday = await this.reservationService.getReservations({ date: this.date });
+    const [first, second, third] = this.reservationsToday;
+    this.reservations = [first, second, third]
+    
+  }
 }
+
--- src/app/pages/dashboard/proximas-reservas/proximas-reservas.component.ts
+++ src/app/pages/dashboard/proximas-reservas/proximas-reservas.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderDashboardComponent } from '../../../components/dashboard/header-dashboard/header-dashboard.component';
 import { ReservationsCardComponent } from '../../../components/dashboard/reservations-card/reservations-card.component';
+import { ReservationService } from '../../../services/reservation.service';
+import { ICustomerReservationResponse } from '../../../interfaces/icustomer-reservation-response.interface';
 
 @Component({
   selector: 'app-proximas-reservas',
@@ -11,4 +13,14 @@ import { ReservationsCardComponent } from '../../../components/dashboard/reserva
 })
 export class ProximasReservasComponent {
 
+  reservationService = inject(ReservationService)
+  reservations: ICustomerReservationResponse[] = []
+
+  ngOnInit() {
+    this.loadData()
+  }
+
+  async loadData() {
+    this.reservations = await this.reservationService.getReservations({})
+  }
 }
