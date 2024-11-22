c37c95f5365b99fabd64df16bd802b9d6d1f5d14
FranFunesMolero <ffunesmolero@icloud.com>
2024-11-22 13:17:55 +0100

    menu conectado

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -4,6 +4,9 @@ import { FooterComponent } from "../../components/footer/footer.component";
 import { ReviewsService } from '../../services/reviews.service';
 import { Comment } from '@angular/compiler';
 import { IUserResponse } from '../../interfaces/user.interfaces';
+import { MenuService } from '../../services/menu.service';
+
+
 
 @Component({
   selector: 'app-menu',
@@ -14,6 +17,28 @@ import { IUserResponse } from '../../interfaces/user.interfaces';
 })
 export class MenuComponent {
 
+  service = inject(MenuService);
+  data?: any;
+  dishes?: any;
+  desserts?: any[];
+  mainDishes?: any[];
+  starters?: any[];
 
+  async ngOnInit() { 
+    const date = new Date().toISOString().split('T')[0];
+    const result = await this.service.getDailyMenu(date);
+    this.data = result;
+    this.dishes = this.data.dishes;
 
+    this.desserts = this.getDishByType(this.dishes, 'dessert');
+    this.mainDishes = this.getDishByType(this.dishes, 'main');
+    this.starters = this.getDishByType(this.dishes, 'starters');
+
+  }
+
+  getDishByType( array: any[], type: string ) {
+  return array.filter((dish: any) => {
+      return dish.type === type
+    });
+  }
 }
