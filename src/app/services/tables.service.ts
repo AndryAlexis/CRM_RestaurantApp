e621638b66a420268818795d18e03a59e25ba4f1
gominada <gominada@proton.me>
2024-11-24 21:29:02 +0100

    Add authorization headers to API calls & QOL changes

--- src/app/services/tables.service.ts
+++ src/app/services/tables.service.ts
@@ -11,25 +11,33 @@ export class TablesService {
   private rootUrl = 'http://localhost:3000/api';
 
   getTables(): Promise<any> {
+    const token = localStorage.getItem('token') as string;
+    const headers = { 'Authorization': token };
     return firstValueFrom(
-      this.http.get<any>(`${this.rootUrl}/tables`));
+      this.http.get<any>(`${this.rootUrl}/tables`, { headers }));
   }
 
   createTable(table: any): Promise<any> {
+    const token = localStorage.getItem('token') as string;
+    const headers = { 'Authorization': token };
     return firstValueFrom(
-      this.http.post<any>(`${this.rootUrl}/tables`, table)
+      this.http.post<any>(`${this.rootUrl}/tables`, table, { headers })
     )
   }
 
   updateTableCapacity(id: number, capacity: number): Promise<any> {
+    const token = localStorage.getItem('token') as string;
+    const headers = { 'Authorization': token };
     return firstValueFrom(
-      this.http.put<any>(`${this.rootUrl}/tables/${id}/capacity/${capacity}`, null)
+      this.http.put<any>(`${this.rootUrl}/tables/${id}/capacity/${capacity}`, null, { headers })
     )
   }
 
   deleteTable(id: number): Promise<any> {
+    const token = localStorage.getItem('token') as string;
+    const headers = { 'Authorization': token };
     return firstValueFrom(
-      this.http.delete<any>(`${this.rootUrl}/tables/${id}`)
+      this.http.delete<any>(`${this.rootUrl}/tables/${id}`, { headers })
     )
   }
 
